1e321cfb9d9a8e162352541883c7bc073d972a0d
kansong-crypto <1013634867@qq.com>
2021-04-11 02:23:18 +0800

    爬虫代码

--- src/main/java/com/example/demo/web/MyIndex.java
+++ src/main/java/com/example/demo/web/MyIndex.java
@@ -2,9 +2,21 @@ package com.example.demo.web;
 
 
 import com.example.demo.config.Config;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpStatus;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.utils.HttpClientUtils;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import sun.net.www.http.HttpClient;
+
+import java.io.IOException;
 
 @RestController
 public class MyIndex {
@@ -14,6 +26,40 @@ public class MyIndex {
 
     @RequestMapping("/index")
     public String index() {
+        return "----------- hello spring boot ----------\n" + config.print();
+    }
+
+    @RequestMapping("/reptile")
+    public String reptile() {
+        //1.生成httpclient，相当于该打开一个浏览器
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+        CloseableHttpResponse response = null;
+        //2.创建get请求，相当于在浏览器地址栏输入 网址
+        HttpGet request = new HttpGet("https://www.baidu.com/");
+        try {
+            //3.执行get请求，相当于在输入地址栏后敲回车键
+            response = httpClient.execute(request);
+
+            //4.判断响应状态为200，进行处理
+            if(response.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
+                //5.获取响应内容
+                HttpEntity httpEntity = response.getEntity();
+                String html = EntityUtils.toString(httpEntity, "utf-8");
+                System.out.println(html);
+            } else {
+                //如果返回状态不是200，比如404（页面不存在）等，根据情况做处理，这里略
+                System.out.println("返回状态不是200");
+                System.out.println(EntityUtils.toString(response.getEntity(), "utf-8"));
+            }
+        } catch (ClientProtocolException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            //6.关闭
+            HttpClientUtils.closeQuietly(response);
+            HttpClientUtils.closeQuietly(httpClient);
+        }
         return "----------- hello spring boot ----------\n" + config.print();
     }
 }
